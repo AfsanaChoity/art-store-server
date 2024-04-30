3a51b76941c0cebda75eb8fc0c69586e45f3eacd
choity <afsanamimi194@gmail.com>
2024-04-30 14:26:40 +0600

    item delete from database

--- index.js
+++ index.js
@@ -88,6 +88,13 @@ async function run() {
       res.send(result)
     })
 
+    //delete item from database
+    app.delete("/delete/:id", async(req, res) =>{
+      const result = await itemCollection.deleteOne({_id: new ObjectId(req.params.id)})
+      console.log(result);
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Successfully connected to MongoDB!");
