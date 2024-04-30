179f4c1c4719e4297e98f78c1b32d9ea93a8f935
choity <afsanamimi194@gmail.com>
2024-04-30 21:08:00 +0600

    connected to localhost

--- index.js
+++ index.js
@@ -15,8 +15,10 @@ app.use(express.json());
 
 
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lbylvoy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+//connected to localhost
 const uri = 'mongodb://localhost:27017';
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
