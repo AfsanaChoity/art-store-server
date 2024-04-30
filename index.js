3623272910cb278e136e91fbb411decdaed0390f
choity <afsanamimi194@gmail.com>
2024-04-30 13:09:11 +0600

    update item in database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -34,21 +34,58 @@ async function run() {
     const userCollection = client.db('artCraft').collection('user');
 
     //user sent to database
-    app.post('/user', async(req, res) =>{
-        const user = req.body;
-        // console.log(user);
-        const result = await userCollection.insertOne(user);
-        res.send(result)
+    app.post('/user', async (req, res) => {
+      const user = req.body;
+      // console.log(user);
+      const result = await userCollection.insertOne(user);
+      res.send(result)
     });
 
     //add product to database
     const itemCollection = client.db('artCraft').collection('items');
-    app.post('/addItem', async(req, res) =>{
+    app.post('/addItem', async (req, res) => {
       const item = req.body;
       const result = await itemCollection.insertOne(item);
       console.log(result);
       res.send(result);
 
+    })
+
+    //get item from database
+    app.get('/myItem/:email', async (req, res) => {
+      // console.log(req.params.email);
+      const result = await itemCollection.find({ email: req.params.email }).toArray();
+      res.send(result)
+    })
+
+    //get single item from database to update item
+    app.get('/singleItem/:id', async (req, res) => {
+      console.log(req.params.id);
+      const result = await itemCollection.findOne({ _id: new ObjectId(req.params.id) })
+      console.log(result)
+      res.send(result)
+    })
+
+    //update item in database
+    app.put('/updateitem/:id', async (req, res) => {
+      console.log(req.params.id);
+      const query = { _id: new ObjectId(req.params.id) };
+      const newData = {
+        $set: {
+          itemName: req.body.itemName,
+            photo: req.body.photo,
+          subcategory: req.body.subcategory,
+            price: req.body.price,
+          rating: req.body.rating,
+            customization: req.body.customization,
+          processingTime: req.body.processingTime,
+            stockStatus: req.body.stockStatus,
+          description: req.body.description
+        }
+      }
+      const result = await itemCollection.updateOne(query, newData);
+      console.log(result);
+      res.send(result)
     })
 
     // Send a ping to confirm a successful connection
@@ -64,10 +101,10 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('art & craft server is running');
+  res.send('art & craft server is running');
 
 })
 
-app.listen(port, () =>{
-    console.log(`server is running on port: ${port}`);
+app.listen(port, () => {
+  console.log(`server is running on port: ${port}`);
 })
